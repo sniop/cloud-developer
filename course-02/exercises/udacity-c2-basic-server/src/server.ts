070112552a27d6ade9bbc1d15316c026e4538f5c
sniop.safe@gmail.com <4bmnRq9p>
2019-12-31 19:19:54 +0000

    initial excercise complete

--- course-02/exercises/udacity-c2-basic-server/src/server.ts
+++ course-02/exercises/udacity-c2-basic-server/src/server.ts
@@ -6,7 +6,7 @@ import { Car, cars as cars_list } from './cars';
 (async () => {
   let cars:Car[]  = cars_list;
 
-  //Create an express applicaiton
+  //Create an express application, init express application server
   const app = express(); 
   //default port to listen
   const port = 8082; 
@@ -68,15 +68,53 @@ import { Car, cars as cars_list } from './cars';
                 .send(`Welcome to the Cloud, ${name}!`);
   } );
 
-  // @TODO Add an endpoint to GET a list of cars
+  // @TODO : DONE Add an endpoint to GET a list of cars
   // it should be filterable by make with a query paramater
+  app.get( "/cars/", ( req: Request, res: Response ) => {
+    let { make } = req.query;
+
+    let cars_list = cars;
+
+    if(make){
+      cars_list = cars.filter(car=>car.make==make);
+    }
+
+    return res.status(200)
+              .send(cars_list);
+  } );
 
   // @TODO Add an endpoint to get a specific car
   // it should require id
   // it should fail gracefully if no matching car is found
+  app.get( "/cars/:carId", 
+    ( req: Request, res: Response ) => {
+      let { carId } = req.params;
+
+      const selectedCar = cars.filter(car=>car.id==carId);
+
+      return res.status(200)
+                .send(selectedCar);
+  } );
 
   /// @TODO Add an endpoint to post a new car to our list
   // it should require id, type, model, and cost
+  app.post( "/cars", 
+    async ( req: Request, res: Response ) => {
+
+      let { make,type,model,cost,id } = req.body;
+
+      if ( !make || !type || !model || !id) {
+        return res.status(400)
+                  .send(`missing car information`);
+      }
+
+      let carRecord: Car = {
+        make:make,type:type,model:model,cost:cost,id:id
+      }
+      cars.push(carRecord);
+      return res.status(200)
+                .send(carRecord);
+  } );
 
   // Start the Server
   app.listen( port, () => {
--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
@@ -1,10 +1,10 @@
-import { add, divide } from './units';
+import { add, divide,concat } from './units';
 
 import { expect } from 'chai';
 import 'mocha';
 
 describe('add function', () => {
-
+''
   it('should add two and two', () => {
     const result = add(2,2);
     expect(result).to.equal(4);
@@ -38,3 +38,19 @@ describe('divide', () => {
 // @TODO try creating a new describe block for the "concat" method
 // it should contain an it block for each it statement in the units.ts @TODO.
 // don't forget to import the method ;)
+describe('concat', () => {
+
+  it('should fail if string one is blank', () => {
+    expect(()=>{ concat('','stringtwo') }).to.throw('empty string')
+  });
+
+  it('should fail if string two is blank', () => {
+    expect(()=>{ concat('stringone','') }).to.throw('empty string')
+  });
+
+  it('concatenate two string', () => {
+    const result = concat('Hello ','World');
+    expect(result).to.equal('Hello World');
+  });
+
+});
