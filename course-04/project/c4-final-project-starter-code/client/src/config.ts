70c5a809e500077fada040a0700f52c4d76c23e0
sniop.safe@gmail.com <4bmnRq9p>
2020-03-30 12:39:21 +0100

    added server and auth0 configuration

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,6 +1,7 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
 const apiId = '...'
-export const apiEndpoint = `https://8dp15l6tli.execute-api.us-east-2.amazonaws.com/dev`
+export const apiEndpoint = `https://rrevnxey3k.execute-api.us-east-2.amazonaws.com/dev`
+// export const apiEndpoint = `http://localhost:3003`
 
 export const authConfig = {
   // DONE: Create an Auth0 application and copy values from it into this map
